e28a4e5329c9e750be708083ca95bc9ceea7984d
Tyler Bach <Tbach2@my.wctc.edu>
2022-04-11 20:31:50 -0700

    Add project files.

---
+++ MorseCode/MainPage.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace MorseCode
+{
+    public partial class MainPage : ContentPage
+    {
+        string DotsAndDashes = "";
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        void Dot(object sender, System.EventArgs e)
+        {
+            DotsAndDashes += ".";
+            DotsDashes.Text = DotsAndDashes;
+        }
+        void Dash(object sender, System.EventArgs e)
+        {
+            DotsAndDashes += "-";
+            DotsDashes.Text = DotsAndDashes;
+        }
+        void Gap(object sender, System.EventArgs e)
+        {
+            if(string.IsNullOrEmpty(DotsAndDashes))
+            {
+                Text.Text += " ";
+            }
+            else
+            {
+                Text.Text += $"{Morse.MorseCoder(DotsAndDashes)}";
+                DotsAndDashes = "";
+            }
+            
+        }
+    }
+}
